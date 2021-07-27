cfe1bdf6c1b02051f92ac0d329d09de1c6e2de36
johnny-rob <jpinney1@gmail.com>
2021-07-27 07:22:44 -0400

    Minor adjustments to text rendering

--- lib/sgdesigns/renderer.rb
+++ lib/sgdesigns/renderer.rb
@@ -2,7 +2,7 @@ module Sgdesigns
   class Error < StandardError; end
   class Renderer
     def self.copyright name, msg
-      "Copyright; #{Time.now.year} | " " <b>#{name}</b> " " #{msg}".html_safe
+      "Copyright; #{Time.now.year} | " " #{name} ". " #{msg}".html_safe
     end
   end
 end
